3d388700af7c6e64571d4055e75c60e53837e8f1
Abdali21 <160420279+Abdali21@users.noreply.github.com>
2024-10-28 14:57:43 +0100

    an issue

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { PORT } from "./config.js";
 
 const app = express();
+app.use(express.json());
 
 app.listen(PORT, ()=>{
     console.log(`server is running on port ${PORT}`);
