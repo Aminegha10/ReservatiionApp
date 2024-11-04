d41f99cb5869f8e8c583a213bfabc372715cb2dc
Abdali21 <160420279+Abdali21@users.noreply.github.com>
2024-11-04 16:01:04 +0100

    add shadcn

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div>
+       <Button variant="outline">click</Button>
+    </div>
+  )
+}
+
+export default App
+
